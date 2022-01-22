4a66ca01ad6584ae9f24461acb82eef4568ea40b
Alexis Gonzalez <alexis.gonzalez07@gmail.com>
2022-01-21 18:50:56 -0800

    Uploaded photo and started new tab bar

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -2,65 +2,76 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
+import alexlogo from "../assets/images/alexlogo.png";
 import "../App.css";
+import "./styles/NavBar.css";
+import Box from "@mui/material/Box";
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
   return (
     <div>
-    <Navbar
-      style={{
-        backgroundColor: "black",
-        // margin: "0em 0em 0em",
-        // padding: "0em 0em",
-      }}
-      expand="lg"
-    >
-      <Container>
-        <Navbar.Brand
-          style={{ color: "white" }}
-          href="#Home"
-          onClick={() => handlePageChange("Home")}
-        >
-          React-Bootstrap
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link
-              style={{ color: "white" }}
-              href="#About-Me"
-              onClick={() => handlePageChange("About-Me")}
-            >
-              About Me
-            </Nav.Link>
-            <Nav.Link
-              style={{ color: "white" }}
-              href="#My-Projects"
-              onClick={() => handlePageChange("My-Projects")}
-            >
-              My Projects
-            </Nav.Link>
-            <Nav.Link
-              style={{ color: "white" }}
-              href="#Contact-Me"
-              onClick={() => handlePageChange("Contact-Me")}
-            >
-              Contact Me
-            </Nav.Link>
-            <Nav.Link
-              style={{ color: "white" }}
-              target="_blank"
-              href="./assets/files/May2021-Resume.pdf"
-              onClick={() => handlePageChange("Resume")}
-            >
-              Resume
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navbar
+        justify
+        variant="tabs"
+        style={{
+          backgroundColor: "black",
+          margin: "0em 0em 0em",
+          padding: "0em 0em",
+        }}
+        fixed="top"
+        expand="lg"
+      >
+        <Container>
+          <Navbar.Brand
+            style={{ color: "white" }}
+            href="#Home"
+            onClick={() => handlePageChange("Home")}
+          >
+            <img
+              class="ui fluid image logo"
+              src={alexlogo}
+              alt="Alex Gonzalez"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link
+                style={{borderColor: "white"}}
+                className="navsegment"
+                href="#About-Me"
+                onClick={() => handlePageChange("About-Me")}
+              >
+                About Me{" "}
+              </Nav.Link>
+              <Nav.Link
+                className="navsegment"
+                href="#My-Projects"
+                onClick={() => handlePageChange("My-Projects")}
+              >
+                My Projects
+              </Nav.Link>
+              <Nav.Link
+                className="navsegment"
+                href="#Contact-Me"
+                onClick={() => handlePageChange("Contact-Me")}
+              >
+                Contact Me
+              </Nav.Link>
+              <Nav.Link
+                style={{ color: "white" }}
+                target="_blank"
+                href="./assets/files/May2021-Resume.pdf"
+                onClick={() => handlePageChange("Resume")}
+              >
+                Resume
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 }
